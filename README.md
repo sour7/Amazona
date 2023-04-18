dc9dba9fb2a6a9ca9f8b587a704f01e19738fd38
sour7 <hellosourabh040@gmail.com>
2023-04-18 17:49:29 +0530

    "initial"

---
+++ README.md
@@ -0,0 +1,1 @@
+# TypeScript mern Amazona
---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./app.css"
+const App = () => {
+  return (
+    <div>
+      <header>Amazona</header>
+      <main>body</main>
+      <footer>fooyeyrtrtrtteerr</footer>
+    </div>
+  )
+}
+
+export default App
